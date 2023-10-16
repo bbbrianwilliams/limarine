ba8975a671bb8318cd61df838e5c50ab599b1002
Brian Williams <120751965+bbbrianwilliams@users.noreply.github.com>
2023-10-16 16:02:42 -0700

    Update Api.js

--- limarinedesktop/src/Api.js
+++ limarinedesktop/src/Api.js
@@ -3,3 +3,5 @@ export const SURF_INFO_API = 'https://marine-api.open-meteo.com/v1/marine?'
 export const WEATHER_API_KEY = 'a09e846b7ad7a06a20e0181a3921b104';
 
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/';
+
+
